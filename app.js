09c6beed8c33d23c957adc2f630714e381543f66
mfadin <m.fadin@ibm.com>
2020-11-03 17:24:35 +0400

    UPDATE change version of app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ http = require("http");
 http
   .createServer(function(req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("Hello! I have been deployed by Fadin -- v5");
+    res.write("Hello! I have been deployed by Fadin -- v6");
     res.end();
   })
   .listen(8080);
